b1f845550b3daf90cc2ba5a6051623b6c57d81f0
Serhii Izotov <sergiyizotov@gmail.com>
2023-02-06 21:42:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
